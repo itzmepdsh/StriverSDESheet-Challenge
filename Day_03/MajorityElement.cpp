9fd2da166c5a3e029b8b8e4bd4e3501fc438f262
itzmepdsh <75720286+itzmepdsh@users.noreply.github.com>
2023-06-09 00:24:06 +0530

    Update MajorityElement.cpp

--- Day_03/MajorityElement.cpp
+++ Day_03/MajorityElement.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int,int> mp;
-        for(int num : nums){
-            mp[num]++;
-        }
-        for(pair<int,int> pr : mp){
-            if(pr.second > n/2){
-                return pr.first;
+        //Time Complexity = O(n) Space Complexity = O(1)
+        int n = nums.size(),element,cnt = 0;
+        for(int i=0;i<n;i++){
+            if(cnt == 0){
+                element = nums[i];
             }
+            if(nums[i] == element)cnt++;
+            else cnt--;
         }
-        return -1;
+        //No need to verify since it is given majority element always exist
+        return element;
+
+        //Time Complexity = O(nlogn) Space Complexity = O(n)
+        // int n = nums.size();
+        // unordered_map<int,int> mp;
+        // for(int num : nums){
+        //     mp[num]++;
+        // }
+        // for(pair<int,int> pr : mp){
+        //     if(pr.second > n/2){
+        //         return pr.first;
+        //     }
+        // }
+        // return -1;
     }
 };
