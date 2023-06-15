4b758f35f01785aded50b50e1c322bdf68b8eabc
itzmepdsh <75720286+itzmepdsh@users.noreply.github.com>
2023-06-16 00:06:14 +0530

    Update and rename LowestCommonAncestorOfABinaryTree.cpp to LowestCommonAncestorOfABinaryTree.cpp

--- Day_18/LowestCommonAncestorOfABinaryTree.cpp
+++ Day_18/LowestCommonAncestorOfABinaryTree.cpp
@@ -19,8 +19,8 @@ public:
         TreeNode* right = lowestCommonAncestor(root->right,p,q);
         //Now, we have both node from right and left. We have to decide what to return from curr node now.
         //if both left and right have null then we will return null 
-        //if exactly one has non-null node then we will that value
-        //if both are non-null it means curr node is tha LCA so will return curr node 
+        //if exactly one has non-null node then we will return that value
+        //if both are non-null it means curr node is the LCA(p,q) so will return curr node 
         if(left == NULL)return right;
         if(right == NULL)return left;
         return root;
